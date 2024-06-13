7357d8ade4b70b22c96ec2c50b5b0d2602b86463
Paul I <scary.lynx@gmail.com>
2024-06-14 00:51:15 +0100

    init

---
+++ src/ERC20Swapper.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface ERC20Swapper {
+    /// @dev swaps the `msg.value` Ether to at least `minAmount` of tokens in `address`, or reverts
+    /// @param token The address of ERC-20 token to swap
+    /// @param minAmount The minimum amount of tokens transferred to msg.sender
+    /// @return The actual amount of transferred tokens
+    function swapEtherToToken(
+        address token,
+        uint minAmount
+    ) external payable returns (uint);
+}
+
+/**
+ * @title ERC20Swapper
+ * @dev Paul Ilves-Fomichov
+ */
+contract ERC20Swapper {}
